f55038007311e8c2c250930f78b2804b2830c09d
baiyihu <baiyihu@creditease.cn>
2026-03-04 16:13:54 +0800

    fix(clash): 将代理服务器域名加入 fake-ip-filter 解决 DNS 循环依赖

--- web/server.js
+++ web/server.js
@@ -540,12 +540,15 @@ dns:
     - "*.lan"
     - "*.local"
     - "*.localhost"
+    - "${host}"
   default-nameserver:
     - 223.5.5.5
     - 119.29.29.29
   nameserver:
     - https://dns.alidns.com/dns-query
     - https://doh.pub/dns-query
+  nameserver-policy:
+    "${host}": "223.5.5.5"
   fallback:
     - https://8.8.8.8/dns-query
     - https://1.1.1.1/dns-query
