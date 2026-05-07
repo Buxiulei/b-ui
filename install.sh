d27e9c1b94050b59f7d8a6185bf9fb41c4381aba
baiyihu <baiyihu@creditease.cn>
2026-05-07 11:27:42 +0800

    feat(install): add residential-helper.sh to download list

--- install.sh
+++ install.sh
@@ -232,6 +232,7 @@ download_all_files() {
         "server/core.sh:${BASE_DIR}/core.sh"
         "server/b-ui-cli.sh:${BASE_DIR}/b-ui-cli.sh"
         "server/update.sh:${BASE_DIR}/update.sh"
+        "server/residential-helper.sh:${BASE_DIR}/residential-helper.sh"
         "web/server.js:${ADMIN_DIR}/server.js"
         "web/package.json:${ADMIN_DIR}/package.json"
         "web/index.html:${ADMIN_DIR}/index.html"
